70f364b9623a67ef7bf9e288c351bb2b40cd34f7
Vuk Tacic <70604613+Zylve@users.noreply.github.com>
2024-10-09 13:40:00 -0700

    Update Gradle Test

--- practicalone/app/src/main/java/com/vuktacic/practicalone/Main.java
+++ practicalone/app/src/main/java/com/vuktacic/practicalone/Main.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class App {
+public class Main {
     private static String[] lines = new String[0];
     private static int itemCount = 0;
     private static double subTotal = 0;
@@ -18,7 +18,7 @@ public class App {
             // Place groceries.txt in the same folder as the JAR. That is what this very long piece of code does.
             // Creates a file object of the location of the JAR, then gets the path of the folder.
             // For some reason Java does not have a nicer way of doing this.
-            String javaFileFolderPath = (new File(App.class.getProtectionDomain().getCodeSource().getLocation().toURI())).getParentFile().getAbsolutePath();
+            String javaFileFolderPath = (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI())).getParentFile().getAbsolutePath();
 
             // Reads the entire file and loads the lines into an array of strings.
             lines = Files.readAllLines(Path.of(javaFileFolderPath, "groceries.txt")).toArray(new String[0]);
